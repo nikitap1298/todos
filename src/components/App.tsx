f86c00a62baaa0bada67ef63b5eab8ef15c8bfa0
Nikita <nikita.pishchugin@gmail.com>
2023-04-20 10:34:08 +0200

    Setup TypeScript

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./Header"
+
+const App = () => {
+  return (
+    <div className="app-div">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./components/App"
+
+const rootElement = document.getElementById("root") as HTMLBaseElement
+const root = createRoot(rootElement)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
