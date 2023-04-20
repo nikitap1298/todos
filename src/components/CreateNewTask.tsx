20c8c1c3f1ac03d125c338e0dfdab2ae38a78c8b
Nikita <nikita.pishchugin@gmail.com>
2023-04-20 13:12:47 +0200

    import Bootstrap

--- src/components/CreateNewTask.tsx
+++ src/components/CreateNewTask.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import { Button } from "react-bootstrap"
 
-const CreateNewTask = () => {
+const CreateNewTask: React.FC = () => {
   return (
     <div className="create-new-task-div">
       <input type="text" placeholder="Add some task" />
-      <button>Add</button>
+      <Button variant="primary">Primary</Button>{" "}
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./components/App"
 
+// For using Bootstrap inside components. Importing the Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+
 const rootElement = document.getElementById("root") as HTMLBaseElement
 const root = createRoot(rootElement)
 
