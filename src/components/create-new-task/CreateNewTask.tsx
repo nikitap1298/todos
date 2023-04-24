aa64a7d43503f4ae6837f8b7c3ab5840246f6311
Nikita <nikita.pishchugin@gmail.com>
2023-04-24 15:58:15 +0200

    Refactor components

--- src/components/create-new-task/CreateNewTask.tsx
+++ src/components/create-new-task/CreateNewTask.tsx
@@ -1,94 +1,37 @@
-import React, { useEffect, useState } from "react"
-import { Form } from "react-bootstrap"
-import { Button } from "react-bootstrap"
-import Task from "../task/Task"
-import AlertComponent from "../alert/AlertComponent"
-import { tasksArrayKey } from "../../constants/constants"
+import React, { useState } from "react"
+import { Form, Button } from "react-bootstrap"
 
-const CreateNewTask = () => {
-  const [tasksArray, setTasksArray] = useState([])
+const CreateNewTask = ({ addNewTask }) => {
   const [newTaskName, setNewTaskName] = useState("")
-  const [showAlert, setShowAlert] = useState(false)
-  const [alertMessage, setAlertMessage] = useState("")
-
-  // Load tasksArray from localStorage
-  useEffect(() => {
-    const tasksArrayLocalStorage = JSON.parse(
-      localStorage.getItem(tasksArrayKey)
-    )
-    if (tasksArrayLocalStorage) {
-      setTasksArray(tasksArrayLocalStorage)
-    }
-  }, [])
 
   const handleNewTaskInputChange = (event) => {
     setNewTaskName(event.target.value)
   }
 
-  const addNewTask = (event) => {
+  const handleAddClick = (event) => {
     // Turn off the refresh of the page after user click 'enter' in form
     event.preventDefault()
-
-    const capitalizedMessage =
-      newTaskName.charAt(0).toUpperCase() + newTaskName.slice(1)
-
-    // User can't add the same task
-    if (!tasksArray.includes(capitalizedMessage) && capitalizedMessage !== "") {
-      setTasksArray((oldArray) => [...oldArray, capitalizedMessage])
-      setShowAlert(false)
-      localStorage.setItem(
-        tasksArrayKey,
-        JSON.stringify([...tasksArray, capitalizedMessage])
-      )
-    } else if (tasksArray.includes(capitalizedMessage)) {
-      setAlertMessage(capitalizedMessage)
-      setShowAlert(true)
-    }
-
+    addNewTask(newTaskName)
+    
     // Delete text from input
     setNewTaskName("")
   }
 
-  const deleteTask = (index) => {
-    const newTasksArray = [...tasksArray]
-    newTasksArray.splice(index, 1)
-    setTasksArray(newTasksArray)
-    localStorage.setItem(tasksArrayKey, JSON.stringify([...newTasksArray]))
-  }
-
-  const renderTaskComponent = tasksArray.map((task) => (
-    <Task
-      key={task}
-      arrayIndex={tasksArray.indexOf(task)}
-      task={task}
-      onDelete={deleteTask}
-    />
-  ))
-
   return (
-    <>
-      {renderTaskComponent}
-      {showAlert ? (
-        <AlertComponent
-          title="This task already exists:"
-          message={alertMessage}
+    <div className="create-new-task">
+      <Form onSubmit={handleAddClick}>
+        <Form.Control
+          type="text"
+          placeholder="Add some task"
+          size="lg"
+          value={newTaskName}
+          onChange={handleNewTaskInputChange}
         />
-      ) : null}
-      <div className="create-new-task">
-        <Form onSubmit={addNewTask}>
-          <Form.Control
-            type="text"
-            placeholder="Add some task"
-            size="lg"
-            value={newTaskName}
-            onChange={handleNewTaskInputChange}
-          />
-          <Button variant="secondary" size="lg" onClick={addNewTask}>
-            Add
-          </Button>
-        </Form>
-      </div>
-    </>
+        <Button variant="secondary" size="lg" onClick={handleAddClick}>
+          Add
+        </Button>
+      </Form>
+    </div>
   )
 }
 
--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -4,16 +4,14 @@ import { Form } from "react-bootstrap"
 const Task = ({ task, onDelete, arrayIndex }) => {
   return (
     <div className="task">
-      <div>
-        <h1>{task}</h1>
-        <Form>
-          <Form.Check
-            type="checkbox"
-            label=""
-            onChange={() => onDelete(arrayIndex)}
-          />
-        </Form>
-      </div>
+      <h1>{task}</h1>
+      <Form>
+        <Form.Check
+          type="checkbox"
+          label=""
+          onChange={() => onDelete(arrayIndex)}
+        />
+      </Form>
       <hr></hr>
     </div>
   )
