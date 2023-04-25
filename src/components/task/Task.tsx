5d5a9e6b3dc24952108ed8f6d028e58674d3466e
Nikita <nikita.pishchugin@gmail.com>
2023-04-25 09:30:59 +0200

    Finish refactoring. Fix Task.tsx

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -4,14 +4,16 @@ import { Form } from "react-bootstrap"
 const Task = ({ task, onDelete, arrayIndex }) => {
   return (
     <div className="task">
-      <h1>{task}</h1>
-      <Form>
-        <Form.Check
-          type="checkbox"
-          label=""
-          onChange={() => onDelete(arrayIndex)}
-        />
-      </Form>
+      <div>
+        <h1>{task}</h1>
+        <Form>
+          <Form.Check
+            type="checkbox"
+            label=""
+            onChange={() => onDelete(arrayIndex)}
+          />
+        </Form>
+      </div>
       <hr></hr>
     </div>
   )
