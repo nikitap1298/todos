5741bd5c01fed221c1aec72aecd1b82efde6b413
Nikita <nikita.pishchugin@gmail.com>
2023-04-27 17:00:49 +0200

    Change the App.tsx and App.scss place

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import Header from "../header/Header"
-import AlertComponent from "../alert/AlertComponent"
-import { TaskContextProvider } from "../../context/TaskContext"
-import Tasks from "../tasks/Tasks"
-import CreateNewTask from "../create-new-task/CreateNewTask"
+import Header from "./components/header/Header"
+import AlertComponent from "./components/alert/AlertComponent"
+import { TaskContextProvider } from "./context/TaskContext"
+import Tasks from "./components/tasks/Tasks"
+import CreateNewTask from "./components/create-new-task/CreateNewTask"
 import "./App.scss"
 
 const App = () => {
