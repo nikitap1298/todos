9db9eef1ed9b9f49b79b2ac8a8f34c52671e1066
Nikita <nikita.pishchugin@gmail.com>
2023-04-28 14:41:55 +0200

    Delete button can be hidden

--- src/components/manage-completed-tasks/ManageCompletedTasks.tsx
+++ src/components/manage-completed-tasks/ManageCompletedTasks.tsx
@@ -1,15 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Button } from "react-bootstrap"
 import { useTaskContext } from "../../context/TaskContext"
 import "./ManageCompletedTasks.scss"
 
 const ManageCompletedTasks = () => {
-  const { deleteCompletedTasks } = useTaskContext()
+  const { tasks, deleteCompletedTasks } = useTaskContext()
+  let [className, setClassName] = useState("manage-completed-tasks-hidden")
+
+  useEffect(() => {
+    if (tasks.some((element) => element.finished === true)) {
+      setClassName("manage-completed-tasks")
+    } else {
+      setClassName("manage-completed-tasks-hidden")
+    }
+  }, [tasks])
 
   return (
-    <div className="manage-completed-tasks">
+    <div className={className}>
       <Button variant="secondary" size="sm" onClick={deleteCompletedTasks}>
-        Delete Tasks
+        Delete completed tasks
       </Button>
     </div>
   )
