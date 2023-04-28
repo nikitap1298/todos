f1ed8ae7202d2906f018531d94887c854c057571
Nikita <nikita.pishchugin@gmail.com>
2023-04-28 18:25:37 +0200

    Improve 'ManageCompletedTasks'

--- src/components/manage-completed-tasks/ManageCompletedTasks.tsx
+++ src/components/manage-completed-tasks/ManageCompletedTasks.tsx
@@ -6,9 +6,18 @@ import "./ManageCompletedTasks.scss"
 const ManageCompletedTasks = () => {
   const { tasks, deleteCompletedTasks } = useTaskContext()
   let [className, setClassName] = useState("manage-completed-tasks-hidden")
+  let [buttonTitle, setButtonTitle] = useState("Delete completed task")
+
+  // Check if objects in "tasks" contains "finished === true"
+  const finishedIsTrue = tasks.filter((element) => element.finished === true)
 
   useEffect(() => {
     if (tasks.some((element) => element.finished === true)) {
+      if (finishedIsTrue.length <= 1) {
+        setButtonTitle("Delete completed task")
+      } else {
+        setButtonTitle("Delete completed tasks")
+      }
       setClassName("manage-completed-tasks")
     } else {
       setClassName("manage-completed-tasks-hidden")
@@ -18,7 +27,7 @@ const ManageCompletedTasks = () => {
   return (
     <div className={className}>
       <Button variant="secondary" size="sm" onClick={deleteCompletedTasks}>
-        Delete completed tasks
+        {buttonTitle}
       </Button>
     </div>
   )
