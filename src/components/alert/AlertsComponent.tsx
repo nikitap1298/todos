1610f7769301275c31c8bd632eb0267f633779f5
Nikita <nikita.pishchugin@gmail.com>
2023-05-04 14:21:37 +0200

    Use uuid as a key in AlertsComponent

--- src/components/alert/AlertsComponent.tsx
+++ src/components/alert/AlertsComponent.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useAlertContext } from "../../context/AlertContext"
 import AlertComponent from "./AlertComponent"
 import { AlertInterface } from "../../lib/interfaces/alert.interface"
+import { v4 as uuidv4 } from "uuid"
 
 export default function AlertsComponent(): JSX.Element {
   const { alerts, deleteAlert } = useAlertContext()
@@ -10,7 +11,7 @@ export default function AlertsComponent(): JSX.Element {
     <div>
       {alerts.map((alert: AlertInterface, index: number) => (
         <AlertComponent
-          key={Math.random()}
+          key={uuidv4()}
           alert={alert}
           onDelete={deleteAlert}
           alertIndex={index}
