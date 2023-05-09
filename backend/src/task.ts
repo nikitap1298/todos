565dd2fa4e67acdf845b22e150c2b940cf2f529a
Nikita <nikita.pishchugin@gmail.com>
2023-05-09 16:08:39 +0200

    Start mongodb setup

--- backend/src/task.ts
+++ backend/src/task.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import { ConnectOptions } from "mongoose"
 import { TaskInterface } from "./lib/interfaces/task.interface"
 
 const app = express()
@@ -12,10 +14,29 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
+// MongoDB
+const url = "mongodb://localhost:27017/todos"
+mongoose.connect(url, {
+  useNewUrlParser: true,
+} as ConnectOptions)
+
+const tasksSchema = new mongoose.Schema({
+  title: String,
+  createdAt: String,
+  finished: Boolean,
+  finishedAt: String,
+})
+
+const Tasks = mongoose.model("Tasks", tasksSchema)
+
 app
   .route("/tasks")
   .get((req, res) => {
-    res.json(tasks)
+    Tasks.find({}).then((tasks) => {
+      res.json(tasks)
+      console.log(tasks)
+      tasks = tasks
+    })
   })
   .post((req, res) => {
     const dataArray = req.body
@@ -28,12 +49,21 @@ app
 
       if (!exists) {
         tasks.push(element)
+
+        const el = new Tasks({
+          title: element.title,
+          createdAt: element.createdAt,
+          finished: element.finished,
+          finishedAt: element.finishedAt,
+        })
+        el.save()
       }
     })
   })
   .put((req, res) => {
     const updatedArray = req.body
     tasks = updatedArray
+    
   })
 
 app.listen(port, () => {
