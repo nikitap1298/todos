1cb3c80f8984036056f386514f3a7ddee8adb72c
Nikita <nikita.pishchugin@gmail.com>
2023-05-23 11:29:51 +0200

    Setup JWT

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,20 +1,19 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common"
 import { UserService } from "../user/user.service"
+import { JwtService } from "@nestjs/jwt"
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
   async signIn(login: string, pass: string): Promise<any> {
     const user = await this.userService.findUser(login)
     if (user?.password != pass) {
       throw new UnauthorizedException()
     }
-    const { password, ...result } = user
-    /* 
-    Generate a JWT and return it here
-    instead of the user object\
-    */
-    return result
+    const payload = { sub: user.userId, login: user.login }
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    }
   }
 }
--- backend/src/user/user.interface.ts
+++ backend/src/user/user.interface.ts
@@ -1,6 +1,7 @@
 import { Document } from "mongoose"
 
 export interface UserInterface extends Document {
+  userId?: string 
   login: string
   password: string
 }
