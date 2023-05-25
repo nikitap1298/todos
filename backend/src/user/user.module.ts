85374254d92341a4f6984f9431b02d64c2ace24d
Nikita <nikita.pishchugin@gmail.com>
2023-05-25 14:09:11 +0200

    Small changes

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,38 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Module } from "@nestjs/common"
 import { MongooseModule } from "@nestjs/mongoose"
 import { UserService } from "./user.service"
 import { UserSchema } from "./user.schema"
 import { UserController } from "./user.controller"
+import { ListSchema } from "../list/list.schema"
+import bcrypt from "bcrypt"
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: "User", schema: UserSchema }]),
-    MongooseModule.forFeature([{ name: "List", schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: "User",
+        useFactory: () => {
+          const schema = UserSchema
+          // Encrypt user password
+          schema.pre("save", async function (next) {
+            const user = this
+            if (!user.isModified("password")) return next()
+
+            try {
+              const salt = await bcrypt.genSalt()
+              const hash = await bcrypt.hash(user.password, salt)
+              user.password = hash
+              next()
+            } catch (error) {
+              return console.error(error)
+            }
+          })
+          return schema
+        },
+      },
+    ]),
+    MongooseModule.forFeature([{ name: "List", schema: ListSchema }]),
   ],
   providers: [UserService],
   exports: [UserService],
