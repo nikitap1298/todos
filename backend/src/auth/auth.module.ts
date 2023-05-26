b401b1a15339be3f6239419d849278b6a1d40b21
Nikita <nikita.pishchugin@gmail.com>
2023-05-26 09:28:26 +0200

    Change JWT expire time to 60minutes

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { TaskModule } from "../task/task.module"
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: "60s" },
+      signOptions: { expiresIn: "60m" },
     }),
   ],
   controllers: [AuthController],
