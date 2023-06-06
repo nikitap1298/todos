a5b2a78b24f9499b2b07c985d870d9dfd940723e
Nikita Pishchugin <nikita.pishchugin@gmail.com>
2023-06-06 14:19:09 +0200

    Added JWT shared secret and ENV's in .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,12 @@ import { AuthModule } from "./auth/auth.module"
 import { UserModule } from "./user/user.module"
 import { MailModule } from "./mail/mail.module"
 import { ConfirmationTokenModule } from './confirmation.token/confirmation.token.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config()
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/todos"),
+    MongooseModule.forRoot(process.env.MONGODB),
     AuthModule,
     UserModule,
     ListModule,
--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -1,13 +1,15 @@
 import { MailerService } from "@nestjs-modules/mailer"
 import { Injectable } from "@nestjs/common"
 import { UserInterface } from "../user/user.interface"
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config()
 
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserConfirmation(user: UserInterface, idAndToken: string) {
-    const url = `http://localhost:3000/auth/confirm/${idAndToken}`
+    const url = `${process.env.CONFIRMATION_URL}${idAndToken}`
 
     await this.mailerService.sendMail({
       to: user.login,
